9a6dfc0e8164743bc30d2d00d31fa9e8e265ecf0
omer <teouysal@outlook.com>
2021-05-25 17:30:10 +0300

    added npm packages

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 
+
+
+
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
 
+app.use(helmet()) //temel http security islemleri, 14tane middeleware bulunur, header ekleriz htst gibi.. 
+
+//api urli altina ayni IP ile 1 saat icinde sadece 100 istek gelebilir
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Bu IP ile cok fazla requestte bulunuldu, 1 saat sonra tekrar deneyiniz'
+  });
+app.use('/api', limiter);
+
+app.use(morgan('dev')); //loglar icin
+app.use(express.json({ limit : '10kb'}));//Body parser ,body'den read islemleri icin  ve 10kb ile sinirlandirdik
+
+app.use(mongoSanitize());//NoSQL query injection onune gecmek icin ekleriz. Bunu eklemeseydik ornegin; login olurken email yerine {"$gt" : ""} yazarak sifre yerinede olan bir sifreyi koydugumuzda login olabiliyoruz
+app.use(xss());//form inputlari ile gelen HTML kodlarini engeller
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => { // bu fonksiyon ile requestte gelen tum fieldler icinden sadece istedigimiz fieldleri ayiklariz
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 
 exports.getAllUsers = catchAsync( async(req, res) => {
 
@@ -16,6 +24,33 @@ exports.getAllUsers = catchAsync( async(req, res) => {
 
 });
 
+exports.updateMe = catchAsync ( async (req,res,next)=>{
+    // 1- Client sifre guncellemeye calisirsa bunun onune geceriz
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'Buradan sadece user bilgileri guncellenebilir, bu route kullanilabilir /updatePassword.',
+          400
+        )
+      );
+    }
+  
+    // 2- Istemedigimiz fieldlari ayiklariz ve sadece guncelleme yapacagimiz fieldlari aliriz
+    const filteredBody = filterObj(req.body, 'name', 'email');
+  
+    // 3- Useri belirledigimiz fieldlere gore update ederiz
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true
+    });
+  
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser
+      }
+    });
+})
 
 
 exports.getUser = (req, res) => {
